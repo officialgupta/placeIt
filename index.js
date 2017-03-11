d476f7bbcb77faf560f8d14b691dd50feb534ff1
Mayank Gupta <mguptamayankg@gmail.com>
2017-03-11 13:38:47 +0000

    Adds initial UI

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var a = 0;
+var path = require('path')
+var express = require('express')
+
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
