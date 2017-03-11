cc49f3a4bcda1841a71cc654cd6a9b2e9fab69ee
surfad <adamditchfield@gmail.com>
2017-03-11 08:15:58 +0000

    added 2 user rooms, auto reconnects

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var a = 0;
-
+var oddhold;
+var pairs = {};
+var loose;
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	a+=1;
-	console.log('a user connected'+a);	
+
+	if(loose){
+		pairs[loose]=socket.id;
+		pairs[socket.id]=loose;
+		loose=null;
+	}else{loose=socket.id;}
+
+	console.log(socket.id);
+
 	socket.on('disconnect', function(){
-		a-=1;
-		console.log('user disconnected'+a);
+		console.log('user disconnected');
+
+		io.to(pairs[socket.id]).emit('chat message', 'user disconnected, findingg new');	
+		if(loose){
+			pairs[loose]=pairs[socket.id];
+			pairs[pairs[socket.id]]=loose;
+			loose=null;
+		}else{loose=pairs[socket.id];
+			pairs[pairs[socket.id]]=null;
+		}
+
+		pairs[socket.id]=null;
 	});
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-    io.emit('chat message', msg);
+		if(pairs[socket.id]){
+			io.to(pairs[socket.id]).emit('chat message', msg);
+		}else{
+
+			if(loose){
+				pairs[loose]=socket.id;
+				pairs[socket.id]=loose;
+				loose=null;
+			}else{loose=socket.id;}
+
+
+			console.log('failed to send, no partner');
+
+		}
 	});
 
 });
